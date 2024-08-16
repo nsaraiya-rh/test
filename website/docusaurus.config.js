9b4f1f24d0bc515758732762efde68ece47ff57b
nsaraiya-rh <93369063+nsaraiya-rh@users.noreply.github.com>
2024-08-16 11:07:40 +1000

    Initial commit

---
+++ website/docusaurus.config.js
@@ -0,0 +1,132 @@
+// Docs at https://docusaurus.io/blog/releases/3.1
+
+
+// Replace 'project-blueprint' with {project name}
+const projectName = 'Project Blueprint'
+// Replace 'project-blueprint' with {project name}
+const projectSlug = 'project-blueprint'
+// Replace 'FINOS' with {name of copyright owner}
+const copyrightOwner = 'FINOS'
+
+module.exports = {
+  title: `FINOS ${projectName}`,
+  tagline: `FINOS ${projectName}`, 
+  url: 'https://finos.org',
+  baseUrl: '/',
+  trailingSlash: false,
+  favicon: 'img/favicon/favicon-finos.ico',
+  projectName: `FINOS ${projectName}`,
+  organizationName: 'FINOS',
+  customFields: {
+    repoUrl: `https://github.com/finos/${projectSlug}`,
+  },
+  scripts: ['https://buttons.github.io/buttons.js'],
+  stylesheets: ['https://fonts.googleapis.com/css?family=Overpass:400,400i,700'],
+  themeConfig: {
+    navbar: {
+      title: `FINOS ${projectName}`,
+      logo: {
+        alt: 'FINOS Logo',
+        src: 'img/favicon/favicon-finos.ico',
+      },
+      items: [
+        {to: 'docs/home', label: 'Docs', position: 'right'},
+        {to: 'docs/roadmap', label: 'Roadmap', position: 'right'},
+        {to: 'docs/team', label: 'Team', position: 'right'},
+        {
+          href: 'https://github.com/finos/',
+          label: 'GitHub',
+          position: 'right',
+        }
+      ],
+    },
+    metadata: [
+      {name: 'description', content: 'FINOS project documentation website blueprint.'},
+      {property: 'og:title', content: 'FINOS Project Blueprint'},
+      {property: 'og:description', content: 'FINOS project documentation website blueprint.'},
+      {property: 'og:image', content: 'https://raw.githubusercontent.com/finos/software-project-blueprint/main/website/static/img/feature/feature-img.png'},
+      {property: 'og:url', content: 'https://project-blueprint.finos.org/'},
+      {name: 'twitter:card', content: 'summary_large_image'},
+      {name: 'twitter:title', content: 'FINOS Project Blueprint'},
+      {name: 'twitter:description', content: 'FINOS project documentation website blueprint.'},
+      {name: 'twitter:image', content: 'https://raw.githubusercontent.com/finos/software-project-blueprint/main/website/static/img/feature/feature-img.png'},
+    ],
+    footer: {
+      copyright: `Copyright © ${new Date().getFullYear()} ${projectName} - ${copyrightOwner}`,
+      logo: {
+        alt: 'FINOS Logo',
+        src: 'img/favicon/favicon-finos.ico',
+        href: 'https://finos.org'
+      },
+      links: [
+        {
+          title: 'Docs',
+          items: [
+            {
+              label: 'Getting Started',
+              to: 'docs/home',
+            },
+            {
+              label: 'Roadmap',
+              to: 'docs/roadmap',
+            },
+            {
+              label: 'Team',
+              to: 'docs/team',
+            }
+          ]
+        },
+        {
+          title: 'FINOS',
+          items: [
+            {
+              label: 'FINOS Website',
+              to: 'https://www.finos.org/',
+            },
+            {
+              label: 'Community Handbook',
+              to: 'https://community.finos.org/',
+            },
+            {
+              label: 'FINOS Projects',
+              to: 'https://landscape.finos.org',
+            }
+          ]
+        },
+        {
+          title: 'About FINOS',
+          items: [
+            {
+              label: 'FINOS Projects on GitHub',
+              to: 'https://github.com/finos',
+            },
+            {
+              label: 'Engage the FINOS Community',
+              to: 'https://www.finos.org/engage-with-our-community',
+            },
+            {
+              label: 'FINOS News and Events',
+              to: 'https://www.finos.org/news-and-events',
+            }
+          ]
+        },
+      ]
+    },
+  },
+  presets: [
+    [
+      '@docusaurus/preset-classic',
+      {
+        docs: {
+          path: '../docs',
+          editUrl:
+            'https://github.com/finos/${projectSlug}/edit/master/website/',
+          sidebarPath: require.resolve('./sidebars.js')
+        },
+        theme: {
+          customCss: require.resolve('./src/css/custom.css'),
+        }
+      }
+    ]
+  ]
+};
